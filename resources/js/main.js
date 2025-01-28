ddae2a1a267758c3c01c66fcd1617cab664e3ae2
henryprosser <henryprosser@yahoo.co.uk>
2025-01-28 18:09:33 +0000

    Tweak nav menu design

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,7 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle");
+navTriangle = document.querySelector(".nav__menu-triangle");
 
 let navClose = false;
 
@@ -10,10 +11,12 @@ if (navToggle) {
     if (!navClose) {
       console.log("toggle clicked: show menu");
       navMenu.classList.add("show-menu");
+      navTriangle.style.opacity = "1"; // Show the triangle
       navClose = true;
     } else {
       console.log("toggle clicked: hide menu");
       navMenu.classList.remove("show-menu");
+      navTriangle.style.opacity = "0"; // Hide the triangle
       navClose = false;
     }
   });
