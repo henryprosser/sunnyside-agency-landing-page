6287753fc6197e0f0cdc9c071885a15f07c1e5b2
henryprosser <henryprosser@yahoo.co.uk>
2025-01-28 19:07:46 +0000

    Animate hamburger menu

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,7 @@
-/*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle");
-navTriangle = document.querySelector(".nav__menu-triangle");
+  navToggle = document.getElementById("nav-toggle"),
+  navTriangle = document.querySelector(".nav__menu-triangle"),
+  hamburger = document.querySelector(".main-nav-toggle");
 
 let navClose = false;
 
@@ -11,11 +11,13 @@ if (navToggle) {
     if (!navClose) {
       console.log("toggle clicked: show menu");
       navMenu.classList.add("show-menu");
+      hamburger.classList.add("active-menu"); // Add active to hamburger
       navTriangle.style.opacity = "1"; // Show the triangle
       navClose = true;
     } else {
       console.log("toggle clicked: hide menu");
       navMenu.classList.remove("show-menu");
+      hamburger.classList.remove("active-menu"); // Remove active from hamburger
       navTriangle.style.opacity = "0"; // Hide the triangle
       navClose = false;
     }
